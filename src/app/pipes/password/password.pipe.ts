3c785157a82d32114be58f4852790fdc31d42f1b
rodrigo.perez <190308@utags.edu.mx>
2020-11-27 21:07:11 -0600

    Commit pipes

---
+++ src/app/pipes/password/password.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'password'
+})
+export class PasswordPipe implements PipeTransform {
+
+  transform(value: string): string {
+    let password = '';
+    for(let i = 0; i < value.length; i++){
+      password += '•';
+    }
+    return password;
+  }
+
+}
